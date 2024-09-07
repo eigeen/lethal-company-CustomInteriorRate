0396ecb04d37252f35124825bc20a979effecafc
Eigeen <dengyk2002@qq.com>
2024-09-07 12:41:33 +0800

    fix build

--- build-package.py
+++ build-package.py
@@ -12,7 +12,7 @@ except:
 manifest = json.load(open("./manifest.json"))
 
 with zipfile.ZipFile(f"./dist/CustomInteriorRate_v{manifest['version_number']}.zip", "w") as zipf:
-    zipf.write("./bin/Release/netstandard2.1/com.eigeen.lethal.CustomInteriorRate.dll")
+    zipf.write("./bin/Release/netstandard2.1/com.eigeen.lethal.CustomInteriorRate.dll", "plugins/com.eigeen.lethal.CustomInteriorRate.dll")
     zipf.write("./README.md")
     zipf.write("./manifest.json")
     zipf.write("./icon.png")
